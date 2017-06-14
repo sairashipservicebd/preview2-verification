65f09dcdbc74a398ca15a7fd074fc54c0487ef7a
Javier Calvarro Nelson <jacalvar@microsoft.com>
2017-06-14 09:37:35 -0700

    Setup ef

---
+++ WebApplication/AppContext.cs
@@ -0,0 +1,19 @@
+﻿using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication
+{
+    public class AppContext : DbContext
+    {
+        public AppContext(DbContextOptions<AppContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+    }
+
+    public class Customer
+    {
+        public int Id { get; set; }
+    }
+}
--- WebApplication/Startup.cs
+++ WebApplication/Startup.cs
@@ -1,29 +1,53 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace WebApplication
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            app.UseDeveloperExceptionPage();
+            app.UseDatabaseErrorPage();
+
+            app.MapWhen(ctx => ctx.Request.Path.StartsWithSegments("/delete"),
+                builder => builder.Run(async (context) =>
+                {
+                    var ctx = context.RequestServices.GetRequiredService<AppContext>();
+                    await ctx.Database.EnsureDeletedAsync();
+                    context.Response.Redirect($"http://{context.Request.Host}");
+                }));
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                var ctx = context.RequestServices.GetRequiredService<AppContext>();
+                var customers = await ctx.Customers.ToListAsync();
+
+                var id = customers.FirstOrDefault() != null ? customers.FirstOrDefault().Id.ToString() : "stranger";
+
+                await context.Response.WriteAsync($"Hello World {id}.");
             });
         }
     }
